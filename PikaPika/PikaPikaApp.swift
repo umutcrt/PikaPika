b7c87b7638510b0736c44d419b60e115cc89ee9d
umutcrt <98687913+umutcrt@users.noreply.github.com>
2022-08-27 09:19:08 +0300

    Initial Commit

---
+++ PikaPika/PikaPikaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PikaPikaApp.swift
+//  PikaPika
+//
+//  Created by Umut Cörüt on 27.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct PikaPikaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
